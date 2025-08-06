9fe51fdcc092e392d41ed1e4444c66513afcb507
victorMK-sys <victorkilonzi0@gmail.com>
2025-08-06 19:11:48 +0300

    Updated folder structure and first commit for assignment

--- python/personalizedGreetings.py
+++
@@ -1,7 +0,0 @@
-name = input('What is your name? ')
-color = input('Which is your favorite color? ')
-
-if len(name) and len(color) > 0:
-  print(f"Hello, {name}! Your favorite color, {color}, is awesome!")
-else:
-  print('Please use valid name and color.')
